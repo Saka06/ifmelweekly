0d7953d3d21922ba49338697de26abdb6bbb9a54
Saka06 <melatisakawuni2812@gmail.com>
2026-06-05 14:33:02 +0700

    Pertemuan

--- index.php
+++ index.php
@@ -19,10 +19,10 @@
     <!-- Menu Navigasi -->
     <table border="1" cellspacing="0" cellpadding="10px">
         <tr>
-            <td><a href="index.html">Home</a></td>
-            <td><a href="profile.html">Profile</a></td>
-            <td><a href="contact.html">Contact</a></td>
-            <td><a href="mahasiswa.html">Data Mahasiswa</a></td>
+            <td><a href="index.php">Home</a></td>
+            <td><a href="profile.php">Profile</a></td>
+            <td><a href="contact.php">Contact</a></td>
+            <td><a href="mahasiswa.php">Data Mahasiswa</a></td>
         </tr>
     </table>
 
@@ -147,9 +147,9 @@
     <!-- Link Internal -->
     <h3>Menu Internal</h3>
 
-    <a href="profile.html">Profile</a> |
-    <a href="contact.html">Contact</a> |
-    <a href="mahasiswa.html">Data Mahasiswa</a>
+    <a href="profile.php">Profile</a> |
+    <a href="contact.php">Contact</a> |
+    <a href="mahasiswa.php">Data Mahasiswa</a>
 
     <br><br>
 
